cc5ad30f78d683c018354bf64c41064b6742e55d
Akankhya <dashakankhya92@gmail.com>
2024-05-17 23:25:52 +0530

    Socket URL added

--- client/src/utils/Api_Routes.js
+++ client/src/utils/Api_Routes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:3002";
-//export const host = "https://chichat-w5fx.onrender.com";
+//export const host = "http://localhost:3002";
+export const host = "https://chichat-w5fx.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const AvatarRoute = `${host}/api/auth/setAvatar`;
