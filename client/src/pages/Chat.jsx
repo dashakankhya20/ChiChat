c347d2a652fae03647435608936e90f3d055a1dd
Akankhya <dashakankhya92@gmail.com>
2024-05-18 12:02:20 +0530

    Solved the issue related to multiple socket connections

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -7,86 +7,100 @@ import { allOtherUsers, host } from '../utils/Api_Routes';
 import Contacts from './Contacts';
 import Welcome from './Welcome';
 import ChatContainer from '../components/ChatContainer';
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
 const Chat = () => {
-    const socket = useRef();
+    const socket = useRef(null); // Initialize socket ref to null
     const navigate = useNavigate();
-    const { user, setUser, isLoggedIn } = useContext(MyContext);
+    const { user } = useContext(MyContext);
     const [contacts, setContacts] = useState([]);
     const [currentChat, setCurrentChat] = useState(undefined);
 
     const handleChangeChat = (chat) => {
         setCurrentChat(chat);
-    }
-    console.log(currentChat);
+    };
 
     useEffect(() => {
         if (!user) {
             navigate("/login");
         }
-    }, []);
+    }, [user, navigate]);
+
     useEffect(() => {
-        if(user) {
-            socket.current = io(host);
-            socket.current.emit("add-user", user._id);
+        if (user && !socket.current) {
+            socket.current = io(host, { autoConnect: false });
+            socket.current.connect();
+            socket.current.on('connect', () => {
+                console.log('Socket connected:', socket.current.id);
+                socket.current.emit("add-user", user._id);
+            });
+            socket.current.on('connect_error', (err) => {
+                console.error('Connection Error:', err);
+            });
         }
-    })
+
+        return () => {
+            if (socket.current) {
+                socket.current.disconnect();
+                socket.current = null;
+            }
+        };
+    }, [user]);
+
     useEffect(() => {
         const getAllOtherUsers = async () => {
-            if (user.isAvatarImageSet) {
-                const data = await axios.get(`${allOtherUsers}/${user._id}`);
-                setContacts(data.data);
-            } else {
+            if (user && user.isAvatarImageSet) {
+                const { data } = await axios.get(`${allOtherUsers}/${user._id}`);
+                setContacts(data);
+            } else if (user) {
                 navigate("/setAvatar");
             }
-        }
+        };
         getAllOtherUsers();
-    }, [])
-    console.log(contacts)
-    console.log("Chat Page", user);
+    }, [user, navigate]);
+
     return (
         <Container>
             <div className='contacts'>
                 <Contacts contacts={contacts} changeChat={handleChangeChat} />
             </div>
             <div className='chat'>
-                {currentChat === undefined ? <Welcome /> : <ChatContainer currentChat={currentChat} socket={socket}/>}
+                {currentChat === undefined ? <Welcome /> : <ChatContainer currentChat={currentChat} socket={socket} />}
             </div>
         </Container>
-    )
-}
+    );
+};
 
 const Container = styled.div`
-    height:100vh;
-    width:100vw;
-    display:flex;
-    flex-direction:row;
-    alignItems:center;
-    justify-content:center;
-    color:white;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    color: white;
     .contacts {
-        width:24vw;
-        height:auto;
-        overflow-y:auto;
-        background-color:black;
-        margin:1rem;
-        border-radius:1rem;
+        width: 24vw;
+        height: auto;
+        overflow-y: auto;
+        background-color: black;
+        margin: 1rem;
+        border-radius: 1rem;
         &::-webkit-scrollbar {
-            width:0.2rem;
+            width: 0.2rem;
             &-thumb {
-                background-color:#ffffff39;
-                width:0.1rem;
-                border-radius:1rem;
+                background-color: #ffffff39;
+                width: 0.1rem;
+                border-radius: 1rem;
             }
         }
     }
     .chat {
-        width:76vw;
-        background-color:black;
-        margin:1rem;
-        border-radius:1rem;
+        width: 76vw;
+        background-color: black;
+        margin: 1rem;
+        border-radius: 1rem;
     }
-
 `;
-export default Chat
+
+export default Chat;
